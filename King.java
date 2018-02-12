a6a97688867bf6ee554b9a7ce11f4404eac0a793
OrbOfOrbit <36204649+OrbOfOrbit@users.noreply.github.com>
2018-02-12 12:55:30 -0500

    Update King.java

--- King.java
+++ King.java
@@ -2,25 +2,16 @@
 public class King extends Piece{
 
 
-	public King(int player){
-
+	public King(int player, String imagePath){
+		super(player, imagePath);
 	}
 
 	public boolean isValidMove(Location from, Location to, Piece[][]b){
-		try{
-			if((to.row == (from.row) || to.row == (from.row + 1) || to.row == (from.row - 1)) && (to.column == (from.column) || to.column == (from.column + 1) || to.column == (from.column - 1)) && !(from.row == to.row && from.column == to.column)  && b[to.row][to.column].getPlayer() != player){
-				return true;
-			}
-			else{
-				return false;
-			}
-		}catch(NullPointerException e){
-			if((to.row == (from.row) || to.row == (from.row + 1) || to.row == (from.row - 1)) && (to.column == (from.column) || to.column == (from.column + 1) || to.column == (from.column - 1)) && !(from.row == to.row && from.column == to.column)){
-				return true;
-			}
-			else{
-				return false;
-			}
+		if((to.row == (from.row) || to.row == (from.row + 1) || to.row == (from.row - 1)) && (to.column == (from.column) || to.column == (from.column + 1) || to.column == (from.column - 1)) && !(from.row == to.row && from.column == to.column)  && b[to.row][to.column].getPlayer() != getPlayer()){
+			return true;
+		}
+		else{
+			return false;
 		}
 	}
 }
