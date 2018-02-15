0015b6f011a996cb898ba71c6165f8c41565def1
UdonNoodleMan <36204425+UdonNoodleMan@users.noreply.github.com>
2018-02-15 00:17:15 -0500

    Add files via upload
    This is the version of Rook that only has vertical movement. This movement works perfectly, with collision and everything.

--- Rook.java
+++ Rook.java
@@ -16,64 +16,42 @@ public class Rook extends Piece{
 	//isValidMove
 	public boolean isValidMove(Location from, Location to, Piece[][]b){
 		boolean finalLocation = false;
-		boolean verticalUp = false, verticalDown = false, horizontalLeft = false, horizontalRight = false;
+        boolean verticalUp = false, verticalDown = false, horizontalLeft = false, horizontalRight = false;
 
-		//This line checks to see if the final destination of the piece contains anything but a friendly piece. This is here, because
-		//anything other than a friendly piece would make the move valid, given that every space in between is blank.
-		if(b[to.getRow()][to.getColumn()].getPlayer() != b[from.getRow()][from.getColumn()].getPlayer())
-			finalLocation = true;
-		else
-			finalLocation = false;
-		
-		//verticalUp
-		if(from.getRow() == to.getRow() && from.getColumn() > to.getColumn()){
-			verticalUp = true;
-			for(int i = to.getColumn(); i < from.getColumn() && verticalUp; i++){
-				if(b[to.getRow()][i].getPlayer() == 0 && verticalUp){
-					verticalUp = true;
-				}
-				else
-					verticalUp = false;
-			}
-		}
-		//verticalDown
-		else if(from.getRow() == to.getRow() && from.getColumn() < to.getColumn()){
-			verticalDown = true;
-			for(int i = from.getColumn(); i < to.getColumn() && verticalDown; i++){
-				if(b[from.getRow()][i].getPlayer() == 0 && verticalDown){
-					verticalDown = true;
-				}
-				else
-					verticalDown = false;
-			}
-		}
-		//horizontalLeft
-		else if(from.getColumn() == to.getColumn() && from.getRow() > to.getRow()){
-			horizontalLeft = true;
-			for(int i = to.getRow(); i < from.getRow() && horizontalLeft; i++){
-				if(b[i][to.getColumn()].getPlayer() == 0 && horizontalLeft){
-					horizontalLeft = true;
-				}
-				else
-					horizontalLeft = false;
-			}
-		}
-		//horizontalRight
-		else if(from.getColumn() == to.getColumn() && from.getRow() < to.getRow()){
-			horizontalRight = true;
-			for(int i = from.getRow(); i < to.getRow() && horizontalRight; i++){
-				if(b[i][from.getColumn()].getPlayer() == 0 && horizontalRight){
-					horizontalRight = true;
-				}
-				else
-					horizontalRight = false;
-			}
-		}
-		
-		if(verticalUp || verticalDown || horizontalLeft || horizontalRight && finalLocation){
-			return true;
-		}
-		else
-			return false;
+        //This line checks to see if the final destination of the piece contains anything but a friendly piece. This is here, because
+        //anything other than a friendly piece would make the move valid, given that every space in between is blank.
+        if(b[to.getRow()][to.getColumn()].getPlayer() != b[from.getRow()][from.getColumn()].getPlayer())
+            finalLocation = true;
+        else
+            finalLocation = false;
+     
+        //verticalUp
+        if(from.getColumn() == to.getColumn() && from.getRow() > to.getRow()){
+            verticalUp = true;
+            for(int i = to.getRow(); i < from.getRow() && verticalUp; i++){
+                if(b[i][to.getColumn()].getPlayer() == 0){
+                    verticalUp = true;
+                }
+                else
+                    return false;
+            }
+        }
+        //verticalDown
+        else if(from.getColumn() == to.getColumn() && from.getRow() < to.getRow()){
+        	verticalDown = true;
+            for(int i = to.getRow(); i > from.getRow() && verticalDown; i--){
+                if(b[i][from.getColumn()].getPlayer() == 0){
+                	verticalDown = true;
+                }
+                else
+                	return false;
+            }
+        }
+        
+        if(verticalUp || verticalDown && finalLocation){
+            return true;
+        }
+        else
+            return false;
 	}
 }
