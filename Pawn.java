3c79d81764483076084a81689b6dd5fe27cbb34c
Zeke-K <36204467+Zeke-K@users.noreply.github.com>
2018-02-14 23:42:19 -0500

    Update Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -5,13 +5,15 @@
 
 public class Pawn extends Piece {
 	// constructors
-	public Pawn(){
+	public Pawn() {
 		super(0, "images/pawn1.png");
 	}
-	public Pawn(int player){
+
+	public Pawn(int player) {
 		super(0);
 	}
-	public Pawn(int player, String imagepath){
+
+	public Pawn(int player, String imagepath) {
 		super(player, imagepath);
 	}
 
@@ -19,129 +21,86 @@ public class Pawn extends Piece {
 	// type: boolean
 	// parameters: Location from, Location to, Piece [][]b
 	// description: Overrides piece's isValidMove method to fit the pawn piece
-	public boolean isValidMove(Location from, Location to, Piece[][]b) {
-		// 		// White
-		// 		// Initial movement: Two spaces in front or one space in front valid -> if spots are open / desired location is not taken
-		// 		if(getPlayer() == 1 && from.getRow() == 6 && !(b[to.getRow()][to.getColumn()].getPlayer() == 1) ||
-		// 				b[to.getRow() - 2][to.getColumn()].getPlayer() == 0 || b[to.getRow() - 1][to.getColumn()].getPlayer() == 0) {
-		// 			return true;
-		// 		}
-
-		// 		// Attacking diagonally - if a black piece is in the position then the move is valid, or if the spot is empty
-		// 		else if(getPlayer() == 1 && b[Math.abs(from.getRow() - to.getRow())][Math.abs(from.getColumn() - to.getColumn())].getPlayer() == 2 || 
-		// 				b[Math.abs(from.getRow() - to.getRow())][Math.abs(from.getColumn() - to.getColumn())].getPlayer() == 0 {
-		// 			return true;
-		// 		}
-
-		// 		// Black
-		// 		// Initial movement: Two spaces in front or one space in front valid -> if spots are open / desired location is not taken
-		// 		if(getPlayer() == 2 && from.getRow() == 1 && !(b[to.getRow()][to.getColumn()].getPlayer() == 2) || 
-		// 				b[to.getRow() + 2][to.getColumn()].getPlayer() == 0 || b[to.getRow() + 1][to.getColumn()].getPlayer() == 0) {
-		// 			return true;
-		// 		}
-
-		// 		// Attacking diagonally - if a white piece is in the position then the move is valid, or if the spot is empty
-		// 		else if(getPlayer() == 2 && b[Math.abs(from.getRow() - to.getRow())][Math.abs(from.getColumn() - to.getColumn())].getPlayer() == 1 || 
-		// 				b[Math.abs(from.getRow() - to.getRow())][Math.abs(from.getColumn() - to.getColumn())].getPlayer() == 0) {
-		// 			return true;
-		// 		}
-
-		// 			// Code 2
-		// 			if(b[to.row][to.column].getPlayer() == getPlayer())
-		// 			return false;
-		// 		if(getPlayer() == 1 && from.row == 6 && to.getRow() == (from.row - 1 | from.row - 2) && from.column == to.column && b[to.row][to.column].getPlayer() == 0)
-		// 				return true;
-		// 		else if(getPlayer() == 1 && to.getRow() == from.row - 1 && from.column == to.column && b[to.row][to.column].getPlayer() == 0)
-		// 			return true;
-		// 		else if(getPlayer() == 1 && to.row == from.row - 1 && to.getColumn() == (from.column + 1 | from.column - 1) && b[to.row][to.column].getPlayer() == 2)
-		// 			return true;
-		// 		else if(getPlayer() == 2 && from.row == 1 && to.getRow() == (from.row + 1 | from.row + 2) && from.column == to.column && b[to.row][to.column].getPlayer() == 0)
-		// 				return true;
-		// 		else if(getPlayer() == 2 && to.getRow() == from.row + 1 && from.column == to.column && b[to.row][to.column].getPlayer() == 0)
-		// 			return true;
-		// 		else if(getPlayer() == 2 && to.row == from.row + 1 && to.getColumn() == (from.column + 1 | from.column - 1) && b[to.row][to.column].getPlayer() == 1)
-		// 			return true;
-
-		// Code 3
-		// White
-		// Cannot move rows originally
-		//		if(getPlayer() == 1 && from.getRow() > to.getRow() || to.getRow() > from.getRow()) {
-		//			return true;
-		//		}
-		//
-		//		// Not taking a piece
-		//		else if(getPlayer() == 1 && from.getColumn() == to.getColumn() && !(b[from.getRow()+1][from.getColumn()].getPlayer() == 0)) {
-		//			return true;
-		//		}
-		//
-		//		// Two spaces in beginning or one space in beginning
-		//		else if(getPlayer() == 1 && Math.abs(to.getRow() - from.getRow()) == 2 ||
-		//				getPlayer() == 1 && Math.abs(to.getRow() - from.getRow()) == 1) {
-		//			return true;
-		//		}
-		//
-		//		// Taking a piece
-		//		if(getPlayer() == 1 && Math.abs(to.getColumn() - from.getColumn()) == 1 || Math.abs(to.getRow() - from.getRow()) == 1 &&
-		//				b[to.getRow()][to.getColumn()].getPlayer() == 0) {
-		//			return true;
-		//		}
-		//		// Can't get piece if it's just in front
-		//		else if(b[to.getRow() - 1][to.column].getPlayer() != getPlayer() || b[to.getRow() - 2][to.getColumn()].getPlayer() != getPlayer()) {
-		//			return false;
-		//		}
-		//
-		//		// Black
-		//		// Cannot move rows originally
-		//		if(getPlayer() == 2 && from.getRow() > to.getRow() || to.getRow() > from.getRow()) {
-		//			return true;
-		//		}
-		//
-		//		// Not taking a piece
-		//		else if(getPlayer() == 2 && from.getColumn() == to.getColumn() && !(b[from.getRow()+1][from.getColumn()].getPlayer() == 0)) {
-		//			return true;
-		//		}
-		//
-		//		// Two spaces in beginning or one space in beginning
-		//		else if(getPlayer() == 2 && Math.abs(to.getRow() - from.getRow()) == 2 ||
-		//				getPlayer() == 2 && Math.abs(to.getRow() - from.getRow()) == 1) {
-		//			return true;
-		//		}
-		//
-		//		// Taking a piece
-		//		if(getPlayer() == 2 && Math.abs(to.getColumn() - from.getColumn()) == 1 || Math.abs(to.getRow() - from.getRow()) == 1 &&
-		//				b[to.getRow()][to.getColumn()].getPlayer() == 0) {
-		//			return true;
-		//		}
-		//		// Can't get piece if it's just in front
-		//		else if(b[to.getRow() + 1][to.column].getPlayer() != getPlayer() || b[to.getRow() + 2][to.getColumn()].getPlayer() != getPlayer()) {
-		//			return false;
-		//		}
-		//		
-		//		return false;
-		System.out.println("cheese" + b[to.row][to.column].getPlayer());
-		if(b[to.row][to.column].getPlayer() == getPlayer())
-			return false;
-		//this works
-		if(getPlayer() == 1 && !(to.equals(new Location(from.row + 1, from.column)) || to.equals(new Location(from.row + 2, from.column)) || to.equals(new Location(from.row + 1, from.column - 1)) || to.equals(new Location(from.row + 1, from.column + 1))))
-			return false;
-		else if(getPlayer() == 1 && to.equals(new Location(from.row + 2, from.column)) && from.row != 1 || b[to.row][to.column].getPlayer() != 0)
-			return false;
-		else if(getPlayer() == 1 && to.equals(new Location(from.row + 1, from.column)) && b[to.row][to.column].getPlayer() != 0)
-			return false;
-		//this is the code for attacking, the third portion of the compound boolean part isn't working right and idk why
-		else if(getPlayer() == 1 && (to.equals(new Location(from.row + 1, from.column - 1)) || to.equals(new Location(from.row + 1, from.column + 1))) && !(b[to.row][to.column].getPlayer() == -1))
-			return false;
+	public boolean isValidMove(Location from, Location to, Piece[][] b) {
 	
-		//this works
-		if(getPlayer() == -1 && !(to.equals(new Location(from.row - 1, from.column)) || to.equals(new Location(from.row - 2, from.column)) || to.equals(new Location(from.row - 1, from.column - 1)) || to.equals(new Location(from.row - 1, from.column + 1))))
+		// return false;
+		System.out.println("cheese" + b[to.row][to.column].getPlayer());
+		if (b[to.row][to.column].getPlayer() == getPlayer()){
+			System.out.println("FALSE");
 			return false;
-		else if(getPlayer() == -1 && to.equals(new Location(from.row - 2, from.column)) && from.row != 6 || b[to.row][to.column].getPlayer() != 0)
+		}
+		
+		// this works
+		 if (getPlayer() == 1
+					&& (to.equals(new Location(from.row + 1, from.column - 1))
+							|| to.equals(new Location(from.row + 1, from.column + 1)))
+					&& !(b[to.row][to.column].getPlayer() == -1)){
+				System.out.println("falSE");
+				return false;
+			}
+			// this is the code for attacking, the third portion of the compound
+			// boolean part isn't working right and idk why
+			else if (getPlayer() == 1
+					&& (to.equals(new Location(from.row + 1, from.column - 1))
+							|| to.equals(new Location(from.row + 1, from.column + 1)))
+					&& (b[to.row][to.column].getPlayer() == -1)){
+				System.out.println("tRRue");
+				return true;
+			}
+		else if (getPlayer() == 1 && !(to.equals(new Location(from.row + 1, from.column))
+				|| to.equals(new Location(from.row + 2, from.column))
+				|| to.equals(new Location(from.row + 1, from.column - 1))
+				|| to.equals(new Location(from.row + 1, from.column + 1)))){
+			System.out.println("FAlse");
 			return false;
-		else if(getPlayer() == -1 && to.equals(new Location(from.row - 1, from.column)) && b[to.row][to.column].getPlayer() != 0)
+		
+			
+		}
+		else if (getPlayer() == 1 && to.equals(new Location(from.row + 1, from.column))
+				&& b[to.row][to.column].getPlayer() != 0){
+			System.out.println("faLSe");
 			return false;
-		//same problem as above
-		else if(getPlayer() == -1 && (to.equals(new Location(from.row - 1, from.column - 1)) || to.equals(new Location(from.row - 1, from.column + 1))) && !(b[to.row][to.column].getPlayer() == 1))
+		}
+
+	
+	
+	
+		
+		// this works
+		else if (getPlayer() == -1
+					&& (to.equals(new Location(from.row - 1, from.column - 1))
+							|| to.equals(new Location(from.row + 1, from.column - 1)))
+					&& (b[to.row][to.column].getPlayer() != 1)){
+				System.out.println("False");
+				return false;
+			}
+		// same problem as above
+			else if (getPlayer() == -1
+					&& (to.equals(new Location(from.row - 1, from.column - 1))
+							|| to.equals(new Location(from.row + 1, from.column - 1)))
+					&& (b[to.row][to.column].getPlayer() == 1)){
+				System.out.println("true");
+				return true;
+			}
+			
+		else if (getPlayer() == -1 && !(to.equals(new Location(from.row - 1, from.column))
+				|| to.equals(new Location(from.row - 2, from.column))
+				|| to.equals(new Location(from.row - 1, from.column - 1))
+				|| to.equals(new Location(from.row - 1, from.column + 1)))){
+			System.out.println("fAlse");
 			return false;
+		}
+
+		else if (getPlayer() == -1 && to.equals(new Location(from.row - 1, from.column))
+				&& b[to.row][to.column].getPlayer() != 0){
+			System.out.println("falSe");
+			return false;
+		}
+		
+		
+	
+		System.out.println("True");
 		return true;
+		
 	}
 }
