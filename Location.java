81a1eede4764b3a933c27498517d59bd6d5f6c30
OrbOfOrbit <36204649+OrbOfOrbit@users.noreply.github.com>
2018-02-14 19:07:14 -0500

    Add files via upload

---
+++ Location.java
@@ -0,0 +1,36 @@
+
+public class Location {
+	public int row;
+	public int column;
+
+	public Location(){
+		this(0,0);
+	}
+	
+	public Location(int r, int c){
+		row = r;
+		column = c;
+	}
+	
+	public int getRow(){
+		return row;
+	}
+	
+	public int getColumn(){
+		return column;
+	}
+	
+	public void setRow(int r){
+		row = r;
+	}
+	
+	public void setColumn(int c){
+		column = c;
+	}
+	
+	public boolean equals(Location l){
+		if(row == l.getRow() && column == l.getColumn())
+			return true;
+		return false;
+	}
+}
