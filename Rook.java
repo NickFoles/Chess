5d912fb9562a529aad34f47da9c6b6f277980177
NickFoles <36204586+NickFoles@users.noreply.github.com>
2018-02-14 20:59:46 -0500

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -23,51 +23,54 @@ public class Rook extends Piece {
 		else
 			finalLocation = false;
 		
-		//verticalUp
+		//horizontal left
 		if(from.getRow() == to.getRow() && from.getColumn() > to.getColumn()){
-			for(int i = to.getColumn(); i < from.getColumn(); i++){
-				if(b[to.getRow()][i].getPlayer() == 0){
-					verticalUp = true;
-				}
-				else
-					verticalUp = false;
-			}
-		}
-		//verticalDown
-		else if(from.getRow() == to.getRow() && from.getColumn() < to.getColumn()){
-			for(int i = from.getColumn(); i < to.getColumn(); i++){
-				if(b[from.getRow()][i].getPlayer() == 0){
-					verticalDown = true;
-				}
-				else
-					verticalDown = false;
-			}
-		}
-		//horizontalLeft
-		else if(from.getColumn() == to.getColumn() && from.getRow() > to.getRow()){
-			for(int i = to.getRow(); i < from.getRow(); i++){
-				if(b[i][to.getColumn()].getPlayer() == 0){
-					horizontalLeft = true;
-				}
-				else
+			for(int i = to.getColumn(); i <= from.getColumn(); i++){
+				if(b[to.getRow()][i].getPlayer() != 0){
 					horizontalLeft = false;
-			}
-		}
-		//horizontalRight
-		else if(from.getColumn() == to.getColumn() && from.getRow() < to.getRow()){
-			for(int i = from.getRow(); i < to.getRow(); i++){
-				if(b[i][from.getColumn()].getPlayer() == 0){
-					horizontalRight = true;
 				}
 				else
+					horizontalLeft = true;
+			}
+		}
+		//horizontal right
+		else if(from.getRow() == to.getRow() && from.getColumn() < to.getColumn()){
+			for(int i = from.getColumn(); i <= to.getColumn(); i++){
+				if(b[from.getRow()][i].getPlayer() != 0){
 					horizontalRight = false;
+				}
+				else
+					horizontalRight = true;
+			}
+		}
+		//vertical up
+		else if(from.getColumn() == to.getColumn() && from.getRow() > to.getRow()){
+			for(int i = to.getRow(); i <= from.getRow(); i++){
+				if(b[i][to.getColumn()].getPlayer() != 0){
+					verticalUp = false;
+				}
+				else
+					verticalUp = true;
+			}
+		}
+		//vertical down
+		else if(from.getColumn() == to.getColumn() && from.getRow() < to.getRow()){
+			for(int i = from.getRow(); i <= to.getRow()+1; i++){
+				if(b[i][from.getColumn()].getPlayer() != 0){
+					verticalDown = false;
+				}
+				
+				else
+					verticalDown = true;
 			}
 		}
 		
-		if((verticalUp || verticalDown || horizontalLeft || horizontalRight && finalLocation) && b[to.row][to.column].getPlayer() != getPlayer()){
+		
+		if((verticalUp || verticalDown || horizontalLeft || horizontalRight) && finalLocation){
 			return true;
 		}
 		else
 			return false;
+	
 	}
 }
